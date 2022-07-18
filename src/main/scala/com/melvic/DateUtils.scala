4088d3a49eab0001bd9cd71f33ad82bd1228a3a5
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-07-19 00:08:14 +0800

    Add sunday counter

---
+++ src/main/scala/com/melvic/DateUtils.scala
@@ -0,0 +1,23 @@
+package com.melvic
+
+import java.time.format.DateTimeFormatter
+import java.time.{DayOfWeek, LocalDate}
+import scala.annotation.tailrec
+
+object DateUtils {
+  def countSundays(from: String, to: String): Int = {
+    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+    val fromDate = LocalDate.parse(from, formatter)
+    val toDate = LocalDate.parse(to, formatter)
+
+    @tailrec
+    def loop(count: Int, fromDate: LocalDate): Int =
+      if (fromDate.isAfter(toDate)) count
+      else {
+        val isSunday = fromDate.getDayOfWeek == DayOfWeek.SUNDAY
+        loop(if (isSunday) count + 1 else count, fromDate.plusDays(1))
+      }
+
+    loop(0, fromDate)
+  }
+}
