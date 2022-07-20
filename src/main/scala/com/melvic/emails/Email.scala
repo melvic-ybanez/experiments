844c53a00d0c96e1f282a544d982605e9c27d243
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2022-07-20 17:57:34 +0800

    Add smart constructor for the email

--- src/main/scala/com/melvic/emails/Email.scala
+++ src/main/scala/com/melvic/emails/Email.scala
@@ -1,10 +1,24 @@
 package com.melvic.emails
 
-final case class Email (local: String, domain: String) {
+final case class Email(local: String, domain: String) {
+
+  /**
+   * Obfuscates an email. The raw email should be converted to lowercase. All the characters in the local
+   * part, except the first and the last, should be replaced by 5 asterisks.
+   */
   lazy val obfuscate: Email = {
     val lowerLocal = local.toLowerCase
     Email(lowerLocal.head + "*****" + lowerLocal.last, domain.toLowerCase)
   }
 
   lazy val show: String = s"$local@$domain"
+}
+
+object Email {
+
+  /**
+   * Creates an email from a raw string.
+   */
+  def fromString(rawEmail: String): Either[String, Email] =
+    EmailParser.parseEmailToEither(rawEmail)
 }
